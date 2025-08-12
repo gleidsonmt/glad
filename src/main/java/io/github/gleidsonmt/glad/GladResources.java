f6fb5f5d602285732670b58d7fbfed2930a2aefe
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-08-12 13:46:57 -0300

    removing unused

--- src/main/java/io/github/gleidsonmt/glad/GladResources.java
+++
@@ -1,9 +0,0 @@
-package io.github.gleidsonmt.glad;
-
-/**
- * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
- * Create on  31/01/2025
- */
-@Deprecated(forRemoval = true)
-public class GladResources {
-}
