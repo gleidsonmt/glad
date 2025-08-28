2f785585f16e321a5cb5d07110c60c20aa9508da
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-08-28 14:38:40 -0300

    update libs

--- src/main/java/io/github/gleidsonmt/glad/App.java
+++ src/main/java/io/github/gleidsonmt/glad/App.java
@@ -20,68 +20,7 @@ public class App extends Application {
 
     }
 
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//
-//        Button test = new Button("Test");
-//
-//        test.setOnMouseClicked(e -> {
-//            test.fireEvent(new ExecutionEventError(new Error("Error"), "Error on shalalala Error on shalalala Error on shalalala Error on shalalala Error on shalalala"));
-//        });
-//
-//        Layout layout = new Layout(test);
-//        Root rootImpl = new Root(layout);
-//
-//        rootImpl.getChildren();
-//
-//        Scene scene = new Scene(rootImpl, 800, 600);
-//
-//        scene.focusOwnerProperty().addListener(new ChangeListener<Node>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Node> observableValue, Node node, Node t1) {
-////                System.out.println("scene focus = " + t1);
-//            }
-//        });
-//
-////        ThemeProvider.install(scene, Font.INSTAGRAM);
-//        ThemeProvider.install(scene, Font.POPPINS, Font.INSTAGRAM);
-//
-//        ThemeProvider.install(scene,
-//                Css.DEFAULT,
-//                Css.TYPOGRAPHIC,
-//                Css.SHAPES,
-//                Css.PROPERTIES,
-//                Css.BOOTSTRAP,
-//                Css.COMBO_BOX,
-//                Css.IMMERSIVE_SCROLL,
-//                Css.TAB_PANE,
-//                Css.PROGRESS_BAR,
-//                Css.HYPERLINK,
-//                Css.LIST_VIEW,
-//                Css.BUTTON,
-//                Css.SPINNER,
-//                Css.CONTEXT_MENU,
-//                Css.TOGGLE_BUTTON,
-//                Css.TOGGLE_SWITCH,
-//                Css.TEXT_FIELD,
-//                Css.CHECK_BOX,
-//                Css.TREE_VIEW,
-//                Css.RADIO_BUTTON,
-//                Css.TEXT_BOX,
-//                Css.DATE_PICKER,
-//                Css.TABLE_VIEW
-//        );
-//
-////        label.setStyle("-fx-font-family: \"Instagram Sans Headline\"; -fx-font-size: 22px; ");
-//
-//        stage.setScene(scene);
-//        stage.show();
-//
-////        scene.getStylesheets().add(getClass().getResource("agents/editor.css").toExternalForm());
-//
-////        CSSFX.start(stage);
-////        ScenicView.show(stage.getScene());
-//    }
+
 
 }
 
