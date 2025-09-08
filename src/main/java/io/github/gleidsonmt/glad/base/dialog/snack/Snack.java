f48c9ac585e6831144c20b08eab82988ea6cc9e7
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-09-08 14:02:39 -0400

    add actions to snack interface

--- src/main/java/io/github/gleidsonmt/glad/base/dialog/snack/Snack.java
+++ src/main/java/io/github/gleidsonmt/glad/base/dialog/snack/Snack.java
@@ -1,7 +1,12 @@
 package io.github.gleidsonmt.glad.base.dialog.snack;
 
 import io.github.gleidsonmt.glad.base.FlowItem;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
+
+import java.util.List;
+
 
 /**
  * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
@@ -12,4 +17,8 @@ public interface Snack extends FlowItem<Snack> {
     Snack message(String message);
 
     Snack graphic(Node graphic);
+
+    Snack action(SnackItem... events);
+
+
 }
