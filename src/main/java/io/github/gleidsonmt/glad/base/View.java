94d26502ae9e79b19bbf196bafa3738698940618
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-09-24 13:03:59 -0300

    update presentation

--- src/main/java/io/github/gleidsonmt/glad/base/View.java
+++ src/main/java/io/github/gleidsonmt/glad/base/View.java
@@ -58,11 +58,7 @@ public class View extends ModuleView implements Module {
 
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("View{");
-        sb.append("\n\tname=").append(getName());
-        sb.append("\n\tcontent=").append(content);
-        sb.append("\n\ticon=").append(icon);
-        sb.append("\n}");
-        return sb.toString();
+        return "View{" + "\n\tname=" + getName() +
+               "\n}";
     }
 }
