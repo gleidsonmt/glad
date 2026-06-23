2ef115df982071911189261748f24e898c629d64
Gleidson Neves <gleidisonmt@gmail.com>
2026-06-23 15:39:34 -0300

    fix slow when scroll is used in presentations

---
+++ src/main/java/io/github/gleidsonmt/glad/App.java
@@ -0,0 +1,20 @@
+package io.github.gleidsonmt.glad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Gleidson Neves da Silveira | <a href="mailto:gleidisonmt@gmail.com">gleidisonmt@gmail.com</a> <br>
+ * Created on 23/06/2026
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
